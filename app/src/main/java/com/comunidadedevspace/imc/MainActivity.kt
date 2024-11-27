b8ec215e2a431952b4b7d04346c9eaf88dff0839
alinedevamorim <a.li.neh@hotmail.com>
2024-11-27 14:51:42 -0300

    Tela Inicial edittext

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.comunidadedevspace.imc
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Recuperar os componentes EditText
+        // Criar uma variavel e associar(=) o componente de UI<EditText)
+        // Recuperar o botão da tela
+
+        // Colocar ação no botao setOnClickListener
+        // Recuperar o texto digitado do edt peso
+
+        val edtPeso = findViewById<TextInputEditText>(R.id.edt_peso)
+        val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
+
+        val btnCalcular = findViewById<Button>(R.id.btn_calcular)
+
+        btnCalcular.setOnClickListener {
+            val peso = edtPeso.text
+            val altura = edtAltura.text
+            println("Aline botão" + altura)
+
+        }
+
+        }
+
+
+
+
     }
-}
